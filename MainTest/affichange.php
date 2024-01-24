002b967bdc278482a13467505f62c1b49cf808a4
ultratavor <felixtonthat64@gmail.com>
2024-01-24 21:07:31 +0100

    affichage des utilisateurs avec pdo state* du mysql

---
+++ MainTest/affichange.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title><p class="upperline">testLevel2 - display</p></title>
+</head>
+
+<body>
+<header>affichange des utilisateurs</header>    
+<?php
+// PDO statement 
+// local uwamp server
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
