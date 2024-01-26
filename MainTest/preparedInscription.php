bded4413ff4b609096482cacb4207a46fd44459a
ultratavor <felixtonthat64@gmail.com>
2024-01-26 17:06:49 +0100

    inscription prep avec pdo

---
+++ MainTest/preparedInscription.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
+
+
+
+<nav class="navL">
+    <a href="./affichange.php">affichange basique</a>
+    <a href="./inscription.php">inscription basique</a>
+    <a href="./preparedaffichange.php"> affichange préparé</a>
+    <a href="./preparedInscription.php">inscription préparée</a>
+</nav> 
+</body>
+</html>
