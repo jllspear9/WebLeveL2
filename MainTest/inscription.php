237d6b55b49022fba472543fbd16676857ad2bb2
ultratavor <felixtonthat64@gmail.com>
2024-01-26 20:07:22 +0100

    inscription non préparée

--- MainTest/inscription.php
+++ MainTest/inscription.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css"/>
     <title class="upperline">testLevel2 - registration</title>
-    <script type="javascript">
+    <script>
         document.getElementById('formulaire').reset();
     </script>
 </head>
@@ -16,12 +16,12 @@
         <!-- bloc formulaire avec légende sur le bord -->
         <fieldset>
             <legend><h1>Registration</h1></legend>
-            <!-- formulaire de saisie -->
-            <form action="./affichange.php" method="POST" id="formulaire">
+            <!-- formulaire de saisie & redirection+envoie dans le fichier affichage-->
+            <form action="./affichage.php" method="POST" id="formulaire">
                 <!-- champs de saisie -->
                 <input type="text" name="txtname" id="name" placeholder="name" require>
                 <input type="text" name="txtsurname" id="surname" placeholder="surname" required>
-                <input type="text" name="txtage" id="age" placeholder="age" required>  
+                <input type="number" name="numage" id="age" placeholder="age" required>  
                 <input type="text" name="txtpassword" id="password" placeholder="password" required>
                 <input type="text" name="txtaddress" id="address" placeholder="address" required>  
                 <input type="submit" name="send" id="send" value="send"> 
@@ -29,7 +29,6 @@
         </fieldset>
     <nav class="navL">
         <a href="./affichange.php">affichange basique</a>
-        <a href="./inscription.php">inscription basique</a>
         <a href="./preparedaffichange.php"> affichange préparé</a>
         <a href="./preparedInscription.php">inscription préparée</a>
     </nav>          
