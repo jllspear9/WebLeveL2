24c917860d40fcba5a2ae51b1e7b7c238b103d9c
ultratavor <felixtonthat64@gmail.com>
2024-01-26 17:06:31 +0100

    inscription non prep

--- MainTest/inscription.php
+++ MainTest/inscription.php
@@ -5,30 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css"/>
     <title class="upperline">testLevel2 - registration</title>
+    <script type="javascript">
+        document.getElementById('formulaire').reset();
+    </script>
 </head>
+
 <body>
+  
     <!-- conteneur de saisie de l'utilisateur -->
         <!-- bloc formulaire avec légende sur le bord -->
-        <fieldset class="customBorder">
-            <legend  class="customBorder"><h1>Registration</h1></legend>
+        <fieldset>
+            <legend><h1>Registration</h1></legend>
             <!-- formulaire de saisie -->
             <form action="./affichange.php" method="POST" id="formulaire">
                 <!-- champs de saisie -->
-                name
-                <input class="champsIn" type="text" name="txtname"      id="name"     placeholder="enter text" require>
-                surname
-                <input class="champsIn" type="text" name="txtsurname"   id="surname"  placeholder="enter text" required>
-                age
-                <input class="champsIn" type="text" name="txtage"       id="age"      placeholder="enter number" required>  
-                password
-                <input class="champsIn" type="text" name="txtpassword"  id="password" placeholder="enter text" required>
-                address
-                <input class="champsIn" type="text" name="txtaddress"   id="address"  placeholder="enter text" required>  
-                <br><br>
-                <input class="champsIn customBorder" type="submit" name="send" id="send" value="send"
-                onclick="document.getElementById('formulaire').reset(); return false;"> 
+                <input type="text" name="txtname" id="name" placeholder="name" require>
+                <input type="text" name="txtsurname" id="surname" placeholder="surname" required>
+                <input type="text" name="txtage" id="age" placeholder="age" required>  
+                <input type="text" name="txtpassword" id="password" placeholder="password" required>
+                <input type="text" name="txtaddress" id="address" placeholder="address" required>  
+                <input type="submit" name="send" id="send" value="send"> 
             </form>
         </fieldset>
+    <nav class="navL">
+        <a href="./affichange.php">affichange basique</a>
+        <a href="./inscription.php">inscription basique</a>
+        <a href="./preparedaffichange.php"> affichange préparé</a>
+        <a href="./preparedInscription.php">inscription préparée</a>
+    </nav>          
     
 </body>
 </html>
