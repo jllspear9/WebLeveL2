fbadf6adc69bf49b46b1df0e8aff566e0ace11ba
ultratavor <felixtonthat64@gmail.com>
2024-01-27 16:54:17 +0100

    affichange des données non préparées

--- MainTest/affichage.php
+++ MainTest/affichage.php
@@ -21,16 +21,24 @@
     // du fichier connexion et aussi afficher les données présente dans la base MySQL
     // dans un site ça ne sera jamais comme ça sauf pour les connexions des développeurs ou admin sys
     include_once("./connexion.php");
-    ///////////// on doit vérifier d'abord que les données ne sont pas déja présentes///////////////
-    $ordreSQL="INSERT INTO user (nom, prenom, age, password0, address0) 
-    VALUES('$name', '$surname', '$age', '$password', '$address')";
-    $nb=$laConnexion->exec($ordreSQL);
-    // vérifications des insertions des données
-    if($nb==0){
-        echo "Oups, Il y a eu une erreur<br>". $laConnexion->errorInfo()[2];
-    }else{
-        echo "Votre compte a bien été ajouté !<br>";
+    //on vérifier d'abord que les données ne sont pas déja présentes
+    $verif="SELECT nom FROM `user` WHERE password0 LIKE '$password' AND address0 LIKE '$address'";
+    $resverif=$laConnexion->query($verif);
+    $resrsql=$resverif->fetch();
+    if ($resrsql == NULL){
+        $ordreSQL="INSERT INTO user (nom, prenom, age, password0, address0) 
+        VALUES('$name', '$surname', '$age', '$password', '$address')";
+        $nb=$laConnexion->exec($ordreSQL);   
+        // vérifications des insertions des données
+        if($nb==0){
+            echo "Oups, Il y a eu une erreur<br>". $laConnexion->errorInfo()[2];
+        }else{
+            echo "Votre compte a bien été ajouté !<br>";
+        }
+    } else {
+        echo "un compte a cette adresse existe déja<br>";           
     }
+
     try{
         // récupération des données de la table 
         $requete="SELECT * FROM user";
@@ -38,6 +46,7 @@
         $tableau=$resultRequest->fetchAll();
         // sécurité sur le nb max d'interations
         $maxIterations=$laConnexion->query("SELECT COUNT(nom) FROM user")->fetch();
+        $maxIterations=$maxIterations[0];
         echo "le nombre de lignes est : $maxIterations";
         // affichange des données de la table 
     } catch (Exception $err){
@@ -45,6 +54,9 @@
     }
     ?>
     <table class="table">
+        <caption>
+            table user (des utilisateurs)
+        </caption>
         <tr> <!-- première ligne (rappel) -->
             <th>index</th> <!-- supplément -->
             <th>nom</th>
@@ -55,11 +67,15 @@
         </tr>
         <!-- boucle php -->
         <?php 
+        // alterner la couleur des lignes (permet de gagner du temps pour visualiser les données)
+        function alternateLine($v){
+            return ($v % 2 == 0) ? 'line0' : 'line1';
+        }
         $idx=0;
         // attention il peut y avoir qu'une seul tuple ou pas du tout (ou un avec une erreur)
         if (!isset($errors[2])){
             while($maxIterations != 0){
-                echo "<tr>
+                echo "<tr class='".alternateLine($idx)."'>
                 <td>".$idx."</td>        
                 <td>".$tableau[$idx]['nom']."</td>
                 <td>".$tableau[$idx]['prenom']."</td>
